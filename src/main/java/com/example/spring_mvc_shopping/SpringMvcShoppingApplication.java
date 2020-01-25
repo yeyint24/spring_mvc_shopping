9b27a096e019f8c5f091e0ed53f5ba382653e3e8
Ye Yint <yeyient242016@gmail.com>
2020-01-25 11:50:24 +0630

    update

---
+++ src/main/java/com/example/spring_mvc_shopping/SpringMvcShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_mvc_shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcShoppingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcShoppingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_mvc_shopping/SpringMvcShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_mvc_shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcShoppingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
